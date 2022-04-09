1440e22e06655dcd1e4741980ecc6919857293c1
danil <vinniopo@mail.ru>
2022-04-09 21:22:09 +0700

    moved to declarative system

--- lesson_4/server_database.py
+++ lesson_4/server_database.py
@@ -1,45 +1,54 @@
-from sqlalchemy import (create_engine, Table, Column, Integer, String,
-                        MetaData, ForeignKey, DateTime, UniqueConstraint)
-from sqlalchemy.orm import mapper, sessionmaker
+from sqlalchemy import (create_engine, Column, Integer, String,
+                        ForeignKey, DateTime, UniqueConstraint)
+from sqlalchemy.orm import sessionmaker, declarative_base
 from common.variables import SERVER_DATABASE
 import datetime
+
+Base = declarative_base()
 
 
 class ServerStorage:
 
     #  таблица для хранения пользователей:
-    class Users:
-        def __init__(self, username):
-            self.id = None
-            self.name = username
-            self.last_login = datetime.datetime.now()
+    class User(Base):
+        __tablename__ = 'users'
+
+        id = Column('id', Integer, primary_key=True)
+        name = Column('name', String, unique=True)
+        last_login = Column('last_login', DateTime)
 
         def __repr__(self):
             return f'User: {self.name}'
 
     # активные пользователи:
-    class ActiveUsers:
-        def __init__(self, user_id, ip_address, port):
-            self.id = None
-            self.user = user_id
-            self.ip_address = ip_address
-            self.port = port
+    class ActiveUser(Base):
+        __tablename__ = 'active_users'
+
+        id = Column('id', Integer, primary_key=True)
+        user = Column('user', ForeignKey('users.id'), unique=True)
+        ip_address = Column('ip_address', String)
+        port = Column('port', Integer)
 
     # история логинов пользователей:
-    class LoginHistory:
-        def __init__(self, user_id, date, ip, port):
-            self.id = None
-            self.user = user_id
-            self.date_time = date
-            self.ip = ip
-            self.port = port
+    class LoginHistory(Base):
+        __tablename__ = 'login_history'
 
-    class ContactList:
-        def __init__(self, owner_id, contact_id):
-            self.id = None
-            self.owner_id = owner_id
-            self.contact_id = contact_id
+        id = Column('id', Integer, primary_key=True)
+        user = Column('user', ForeignKey('users.id'))
+        date_time = Column('date_time', DateTime)
+        ip = Column('ip', String)
+        port = Column('port', String)
 
+    # список контактов:
+    class ContactList(Base):
+        __tablename__ = 'contact_list'
+
+        id = Column('id', Integer, primary_key=True)
+        owner_id = Column('owner_id', ForeignKey('User.id'))
+        contact_id = Column('contact_id', ForeignKey('User.id'))
+
+        UniqueConstraint('owner_id', 'contact_id',
+                         name='contacts_unique_constraint')
 
     def __init__(self):
 
@@ -47,58 +56,22 @@ class ServerStorage:
                                              echo=False,
                                              pool_recycle=60 * 60 * 2)
 
-        self.metadata = MetaData()
-
-        users_table = Table('Users', self.metadata,
-                            Column('id', Integer, primary_key=True),
-                            Column('name', String, unique=True),
-                            Column('last_login', DateTime)
-                            )
-
-        active_users_table = Table('Active_users', self.metadata,
-                                   Column('id', Integer, primary_key=True),
-                                   Column('user', ForeignKey('Users.id'),
-                                          unique=True),
-                                   Column('ip_address', String),
-                                   Column('port', Integer),
-                                   )
-
-        user_login_history = Table('Contact_list', self.metadata,
-                                   Column('id', Integer, primary_key=True),
-                                   Column('user', ForeignKey('Users.id')),
-                                   Column('date_time', DateTime),
-                                   Column('ip', String),
-                                   Column('port', String)
-                                   )
-
-        contacts = Table('Contacts', self.metadata,
-                          Column('id', Integer, primary_key=True),
-                          Column('owner_id', ForeignKey('Users.id')),
-                          Column('contact_id', ForeignKey('Users.id')),
-                          UniqueConstraint('owner_id', 'contact_id',
-                                          name='contacts_unique_constraint')
-                          )
-
+        self.metadata = Base.metadata
         self.metadata.create_all(self.database_engine)
-        mapper(self.Users, users_table)
-        mapper(self.ActiveUsers, active_users_table)
-        mapper(self.LoginHistory, user_login_history)
-        mapper(self.ContactList, contacts)
 
         Session = sessionmaker(bind=self.database_engine)
         self.session = Session()
-        self.session.query(self.ActiveUsers).delete()
+        self.session.query(self.ActiveUser).delete()
         self.session.commit()
 
     def user_login(self, username, ip_address, port):
         """записывает в базу информацию о логине пользователя"""
-        print(username, ip_address, port)
-        res = self.session.query(self.Users).filter_by(name=username)
+        res = self.session.query(self.User).filter_by(name=username)
 
         if res.count():
             user = res.first()
         else:
-            user = self.Users(username)
+            user = self.User(name=username)
             self.session.add(user)
 
         user.last_login = datetime.datetime.now()
@@ -107,46 +80,50 @@ class ServerStorage:
 
     def add_to_active(self, user, ip_address, port):
         """добавляет пользователя в список активных"""
-        new_active_user = self.ActiveUsers(user.id, ip_address, port)
+        new_active_user = self.ActiveUser(user=user.id,
+                                          ip_address=ip_address,
+                                          port=port)
         self.session.add(new_active_user)
-        history = self.LoginHistory(user.id, datetime.datetime.now(),
-                                    ip_address, port)
+        history = self.LoginHistory(user=user.id,
+                                    date_time=datetime.datetime.now(),
+                                    ip=ip_address,
+                                    port=port)
         self.session.add(history)
         self.session.commit()
 
 
     def user_logout(self, username):
 
-        user = self.session.query(self.Users).filter_by(name=username).first()
-        self.session.query(self.ActiveUsers).filter_by(user=user.id).delete()
+        user = self.session.query(self.User).filter_by(name=username).first()
+        self.session.query(self.ActiveUser).filter_by(user=user.id).delete()
         self.session.commit()
 
     def users_list(self):
         # Запрос списка пользователей.
         query = self.session.query(
-            self.Users.name,
-            self.Users.last_login
+            self.User.name,
+            self.User.last_login
         )
         return query.all()
 
     def active_users_list(self):
         # Запрос списка активных пользователей.
         query = self.session.query(
-            self.Users.name,
-            self.Users.last_login,
-            self.ActiveUsers.ip_address,
-            self.ActiveUsers.port,
-        ).join(self.Users)
+            self.User.name,
+            self.User.last_login,
+            self.ActiveUser.ip_address,
+            self.ActiveUser.port,
+        ).join(self.User)
         return query.all()
 
     def login_history(self, username=None):
-        query = self.session.query(self.Users.name,
+        query = self.session.query(self.User.name,
                                    self.LoginHistory.date_time,
                                    self.LoginHistory.ip,
                                    self.LoginHistory.port
-                                   ).join(self.Users)
+                                   ).join(self.User)
         if username:
-            query = query.filter(self.Users.name == username)
+            query = query.filter(self.User.name == username)
         return query.all()
 
     def create_contact(self, owner_id, contact_id):
